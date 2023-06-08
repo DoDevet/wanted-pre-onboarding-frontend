69f8e949a6b57bc1be960c436ac9cdd7c588b13d
Telescope <telescope@Telescopeui-MacBookAir.local>
2023-06-08 14:42:11 +0900

    Delete Layout Hook

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,4 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import isLoggedInFN from "../libs/isLoggedIn";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (isLoggedInFN()) {
-      navigate("/todo");
-    }
-  }, []);
   return (
     <div className="flex items-center justify-center w-full mt-60">
       <div className="w-full max-w-xl">{children}</div>
